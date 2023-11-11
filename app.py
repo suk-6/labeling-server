0e4f1c32c6feb70a48646fa077dd0dd059c68fb1
suk-6 <me@suk.kr>
2023-11-11 09:09:32 +0900

    Fix: Add Image Count

--- app.py
+++ app.py
@@ -4,35 +4,45 @@ import base64
 
 app = Flask(__name__)
 
-if not os.path.exists('images'):
-    os.mkdir('images')
+imageCount = 0
+
+if not os.path.exists("images"):
+    os.mkdir("images")
 else:
-    confirm = input('Remove all files in images folder? (y/n): ')
-    if confirm == 'y':
-        for file in os.listdir('images'):
-            os.remove(f'images/{file}')
+    confirm = input("Remove all files in images folder? (y/n): ")
+    if confirm == "y":
+        for file in os.listdir("images"):
+            os.remove(f"images/{file}")
+    else:
+        imageCount = len(os.listdir("images"))
 
-@app.route('/')
+
+@app.route("/")
 def index():
-    return jsonify({'message': 'OK'})
+    return jsonify({"message": "OK"})
 
-@app.route('/api/image', methods=['POST'])
+
+@app.route("/api/image", methods=["POST"])
 def image():
     try:
+        global imageCount
         data = request.get_json()
-        slider = data['slider']
-        base64Image = data['image']
-        imageExtension = data['extension']
+        slider = data["slider"]
+        base64Image = data["image"]
+        imageExtension = data["extension"]
         print(slider)
 
-        with open(f'images/{slider}.{imageExtension}', 'wb') as f:
+        with open(f"images/{imageCount}.{slider}.{imageExtension}", "wb") as f:
             f.write(base64.b64decode(base64Image))
 
-        return jsonify({'message': 'OK'})
-    
+        imageCount += 1
+
+        return jsonify({"message": "OK"})
+
     except Exception as e:
         print(e)
-        return jsonify({'message': 'Error'})
+        return jsonify({"message": "Error"})
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=10001)
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=10001)
